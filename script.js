dc9bd898cada67763c2028db16a9bb87e1969a58
Peng Jiang <kousiec@icloud.com>
2025-03-23 11:56:44 +0800

    Initial website commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', e => {
+        e.preventDefault();
+        document.querySelector(anchor.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
+    });
+});
